8166ac3591a378eea2359ed847d613d87abc5e8b
alunoinf <alunoinf@pc-649301.(none)>
2015-09-24 22:05:33 -0300

    Exercicio

--- luhn/src/main/java/br/ufg/inf/es/construcao/luhn/Luhn.java
+++ luhn/src/main/java/br/ufg/inf/es/construcao/luhn/Luhn.java
@@ -4,4 +4,24 @@ package br.ufg.inf.es.construcao.luhn;
  * Created by alunoinf on 24/09/15.
  */
 public class Luhn {
+    public static int luhn(int[]n){
+        if (n.length < 2){
+            throw new IllegalArgumentException("k invalido");
+        }
+        int i = 0;
+        int p = 0;
+        int l = n.length -1;
+        boolean a = false;
+        while (l > 0){
+            if (a){
+                p = p + n[l];
+            } else {
+                int t = n[l] * 2;
+                i = i +(t / 10) + (t % 10);
+            }
+            l = l - 1;
+            a = ! a;
+        }
+        return 10 - ((i + p) % 10);
+    }
 }
--- luhn/src/test/java/br/ufg/inf/es/construcao/luhn/LuhnTest.java
+++ luhn/src/test/java/br/ufg/inf/es/construcao/luhn/LuhnTest.java
@@ -1,7 +1,17 @@
 package br.ufg.inf.es.construcao.luhn;
 
-/**
- * Created by alunoinf on 24/09/15.
- */
+import org.junit.Assert;
+
 public class LuhnTest {
+    @org.junit.Test(expected = IllegalArgumentException.class)
+    public void ParametroInvalido() throws Exception {
+        int []d = {3};
+        Luhn.luhn(d);
+    }
+
+    @org.junit.Test
+    public void testCasoTrivial() {
+        int []d = {4, 9, 8, 4, 2, 3, 7, 0, 4, 8, 2, 2, 4, 8, 0, 2};
+        Assert.assertEquals(8, Luhn.luhn(d));
+    }
 }
