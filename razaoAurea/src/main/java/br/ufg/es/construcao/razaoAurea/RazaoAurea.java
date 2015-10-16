e07403a0026997bd1b2a6da484e6ae0fcf7a2b64
alunoinf <alunoinf@pc-649301.(none)>
2015-10-15 21:27:14 -0300

    Aula15/10/2015

---
+++ razaoAurea/src/main/java/br/ufg/es/construcao/razaoAurea/RazaoAurea.java
@@ -0,0 +1,31 @@
+package br.ufg.es.construcao.razaoAurea;
+
+/**
+ * Classe que implementa a Razão-Áurea.
+ */
+public class RazaoAurea {
+
+    /**
+     * Calcula a Razão-Áurea.
+     *
+     * @param t1 entrada do primeiro numero maior que 0.
+     * @param t2 entrada do segundo numero maior que o.
+     * @param fator Precisão do calculo.
+     * @return número ouro (Resultado da Razão-Áurea).
+     */
+
+    public static double razaoAurea(int t1, int t2, int fator){
+        if (t1 < 0 || t2 < 0 || fator < 0) {
+            throw new IllegalArgumentException("t1, t2 ou fator invalido");
+        }
+        double tc = t2;
+        double ta = t1;
+        int i = 0;
+        while ( i <= fator) {
+            tc = tc + ta;
+            ta = tc -ta;
+            i = i + 1;
+        }
+        return tc/ta;
+    }
+}
---
+++ razaoAurea/src/test/java/br/ufg/es/construcao/razaoAurea/RazaoAureaTest.java
@@ -0,0 +1,29 @@
+package br.ufg.es.construcao.razaoAurea;
+
+
+import junit.framework.Assert;
+import org.junit.Test;
+
+public class RazaoAureaTest {
+
+    @Test(expected = IllegalArgumentException.class)
+    public void PrimeiroParamentroInvalido() throws Exception {
+            RazaoAurea.razaoAurea(-1, 2, 5);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void SegundoParamentroInvalido() throws Exception {
+        RazaoAurea.razaoAurea(1, -6, 5);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void TerceiroParamentroInvalido() throws Exception {
+        RazaoAurea.razaoAurea(1, 2, -5);
+    }
+
+    @Test
+    public void CasosDeFronteiras() {
+        Assert.assertEquals(1.61803398875, RazaoAurea.razaoAurea(1, 3, 500), 0.00001d);
+    }
+
+}
